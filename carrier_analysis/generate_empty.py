1b568c48f0896c8291d0c6ae1c435ce5d8b90662
Jakob <jakobfischer93@gmail.com>
2017-12-01 18:17:49 +0100

    Can't get hold on the empty problem. Maybe leaving it at this point.

--- carrier_analysis/generate_empty.py
+++ carrier_analysis/generate_empty.py
@@ -7,31 +7,39 @@ towns = ["t1", "t1"]
 depositories = ["f1", "f2", "w1", "w2", "e1"]
 predicates = ["receiving1", "receiving2", "shipping"]
 
-def try_it(towns, depositories, predicates, min_count, max_count):
-    count = random.randint(min_count, max_count)
-    done = []
-    with open("test.obs", "w") as f:
-        while len(done) < count:
-            p = random.choice(predicates)
-            t = random.choice(towns)
-            d = random.choice(depositories)
-            v = random.randint(0, 1)
-            mask = p + t + d + str(v)
-            if mask not in done:
-                measurement = "%s(%s, %s, %d)." % (p, t, d, v)
-                f.write(measurement + "\n")
-                done.append(mask)
 
+def add_random_measurement(towns, depositories, predicates, min_count, max_count, data = set([])):
+    count = random.randint(min_count, max_count)
+    count += len(data)
+
+    while len(data) < count:
+        p = random.choice(predicates)
+        t = random.choice(towns)
+        d = random.choice(depositories)
+        v = random.randint(0, 1)
+        mask = (p, t, d, v)
+        data.add(mask)
+
+    with open("test.obs", "w") as f:
+        for mask in data:
+            measurement = "%s(%s, %s, %d)." % mask
+            f.write(measurement + "\n")
+    return data
 
 i = 0
+lastSuccess = []
+lastLineCount = 100000000000
 while True:
     i += 1
-    try_it(towns, depositories, predicates, 5, 30)
+    data = add_random_measurement(towns, depositories, predicates, 1, 2)
     out = subprocess.check_output(["dlv", "-silent", "-N=2", "-FD", "abd_carrier.dl", "abd.hyp", "abd_cstr.dl", "abd_fault.dl", "test.obs"])
     out = out.splitlines()
-
-    if len(out) == 1:
-        print out
-        break
-    if i % 1000 == 0:
+    if "Diagnosis:" in out:
+        print "got empty, output line count: ", len(out)
+        if len(out) < lastLineCount:
+            lastSuccess = data
+            lastLineCount = len(out)
+        if len(out) == 1:
+            break
+    if i % 100 == 0:
         print i
